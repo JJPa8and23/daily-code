7ffb428126723d359f1709a043a6af873f971ad3
JJPa8and23 <102738788+JJPa8and23@users.noreply.github.com>
2022-08-09 22:52:08 +0800

    Add files via upload

--- c语言文件操作.c
+++ c语言文件操作.c
@@ -1,18 +1,39 @@
 # define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-int main()
-{
-	int a = 10000;
-	FILE* pf = fopen("test.txt", "wb");      //wb指只写，为此会建立一个test.txt的文件
-	fwrite(&a, 4, 1, pf);      
-	//写入的数据
-	//元素的大小，单位指字节
-	//写入元素的个数
-	//目标文件地址
-	fclose(pf);
-	//关闭指针
-	pf = NULL;  //变为空指针
-	return 0;
-}
+//int main()
+//{
+//	int a = 10000;
+//	FILE* pf = fopen("test.txt", "wb");      //wb指只写，为此会建立一个test.txt的文件
+//	fwrite(&a, 4, 1, pf);      
+//	//写入的数据
+//	//元素的大小，单位指字节
+//	//写入元素的个数
+//	//目标文件地址
+//	fclose(pf);
+//	//关闭指针
+//	pf = NULL;  //变为空指针
+//	return 0;
+//}
+
+//fputc 和fgetc的使用
+//输出和输入一个字符
+
+//fputs 和 fgets 的使用
+//输出和输入一行字符串
+//注意点：有没有换行
+
+//gets 和 gets的使用
+//操作键盘和屏幕
+//从键盘读取一行文本信息
+
+//fscranf 和 fprint 的使用
+//格式化输入和输出函数
+//输入到屏幕上 or 文件上面
 
 
+//对比一组函数
+//scanf/fscanf/sscanf
+//printf/fprintf/sprintf
+
+
+//sscanf 和 sprintf 从字符串里面读和获取
